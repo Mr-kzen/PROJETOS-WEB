ea220c379dca649dd3848fb175f7710cd0e3ae5d
Mr-kzen <lukaszzzen@gmail.com>
2022-01-19 15:26:59 -0300

    Adicionando o projeto Calculadora! Finalizado a parte de Front, falta configurar as suas funcionalidades!

---
+++ Calculadora/Script/script.js
@@ -0,0 +1,11 @@
+/**Funções especiais: 
+ * MC e MR tem que ficar com as cores cinzas até que a função especial for ativada
+ * Os botão terão o click lidos e cada click será mostrado na tela
+ * O teclado só pode armazenar um numero novo após o caracter especial for escolhido
+ * Lembrando que o numero muda se o caracter especial não for escolhido ou ele não for apagado
+ * A tela tem a finalidade de mostrar o que o usuário está fazendo
+*/
+
+
+
+/**O historico ficará com a função de mostrar os resultados junto com a soma após o botão igual ser presionado */
