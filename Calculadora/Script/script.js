07ce7cb5e4b8b394c14b00be7fa0509a25f2267c
Mr-kzen <lukaszzzen@gmail.com>
2022-02-02 07:18:55 -0300

    JS - Atualizando as funcionalidades basicas da calculadora

--- Calculadora/Script/script.js
+++ Calculadora/Script/script.js
@@ -6,6 +6,41 @@
  * A tela tem a finalidade de mostrar o que o usuário está fazendo
 */
 
+function calcular(num){// armazena os valores
+    var numero = document.querySelector("#result").innerHTML
+
+    document.querySelector("#result").innerHTML = numero + num
+
+}
+
+function result(){//retorna o resultado final
+    var resultado = document.querySelector("#result").innerHTML
+
+    if(resultado){
+        document.querySelector("#result").innerHTML = eval(resultado);
+    }else{
+        document.querySelector("#result").innerHTML = ""
+    }
+}
+
+function clean(){
+    document.querySelector("#result").innerHTML = "";
+}
+
+function back(apagar){
+
+    //CE - Apaga tudo após o caracter de soma
+    //Delete - Apaga 1 por 1
+
+    limpar = document.querySelector("#result").innerHTML
+
+    if(apagar == "delete"){
+        document.querySelector("#result").innerHTML = limpar.substring(0, limpar.length -1)
+    }else{
+        limpar.search
+    }
+    
+}
 
 
 /**O historico ficará com a função de mostrar os resultados junto com a soma após o botão igual ser presionado */
