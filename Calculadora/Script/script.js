391f08c239e36ddccae3bab20b122c9572511737
Mr-kzen <lukaszzzen@gmail.com>
2022-02-13 13:18:40 -0300

    Atualizando codigo referente a funcionalidas de historicos

--- Calculadora/Script/script.js
+++ Calculadora/Script/script.js
@@ -6,41 +6,85 @@
  * A tela tem a finalidade de mostrar o que o usuário está fazendo
 */
 
-function calcular(num){// armazena os valores
-    var numero = document.querySelector("#result").innerHTML
+const valor = 0
+const caracter = ['+', '-', '*', '/']
 
-    document.querySelector("#result").innerHTML = numero + num
+function calcular(click){// imprime o que está sendo feito!
 
-}
-
-function result(){//retorna o resultado final
-    var resultado = document.querySelector("#result").innerHTML
-
-    if(resultado){
-        document.querySelector("#result").innerHTML = eval(resultado);
-    }else{
-        document.querySelector("#result").innerHTML = ""
+    if(click == "."){
+        click =`,`
     }
-}
-
-function clean(){
-    document.querySelector("#result").innerHTML = "";
-}
-
-function back(apagar){
-
-    //CE - Apaga tudo após o caracter de soma
-    //Delete - Apaga 1 por 1
-
-    limpar = document.querySelector("#result").innerHTML
-
-    if(apagar == "delete"){
-        document.querySelector("#result").innerHTML = limpar.substring(0, limpar.length -1)
-    }else{
-        limpar.search
+    if(document.querySelector("#result").innerHTML.search('=')>0){
+        document.querySelector("#result").innerHTML = ""
+        document.querySelector("#resultclick").innerHTML = ""
+    }
+    if(click == "+" || click == "-" || click == "*" || click == "/"){
+        document.querySelector("#result").innerHTML += document.querySelector("#resultclick").innerHTML + click
+        document.querySelector("#resultclick").innerHTML = ""
+    }else if(click){
+        document.querySelector("#resultclick").innerHTML += click
     }
     
 }
 
+function result(){//retorna o resultado final
+    
+    document.querySelector("#result").innerHTML += document.querySelector("#resultclick").innerHTML
+    
+    const resultClick = document.querySelector("#resultclick").innerHTML
+    var resultado = document.querySelector("#result").innerHTML 
+    
+    resultado.replace(",", ".");
+    // resultado.replace("=", "");
 
-/**O historico ficará com a função de mostrar os resultados junto com a soma após o botão igual ser presionado */
+    if(resultado){
+        document.querySelector("#resultclick").innerHTML = eval(resultado)
+        document.querySelector("#result").innerHTML += '='
+
+        // historico
+
+            //-- Criando os elementos que serão inseridos
+
+                const container = document.querySelector(".historico")
+
+                const novaDiv = document.createElement("div")
+
+                const p1 = document.createElement("p")       
+                p1.appendChild(document.createTextNode(resultado))
+                novaDiv.appendChild(p1)
+
+                const p2 = document.createElement("p")
+                p2.appendChild(document.createTextNode(eval(resultado)))
+                novaDiv.appendChild(p2)
+                
+                container.appendChild(novaDiv)
+
+                document.querySelector("#historicos").classList.add('sr-only')
+                novaDiiv.classList.add('historicos')
+    }
+    else{
+        document.querySelector("#resultclick").innerHTML = ""
+    }
+}
+
+function clean(botao){
+
+    limpar = document.querySelector("#resultclick").innerHTML
+
+    if(botao == 'c'){
+        document.querySelector("#resultclick").innerHTML = ""
+        document.querySelector("#result").innerHTML = ""
+    }
+    else if(botao == "delete"){
+        document.querySelector("#resultclick").innerHTML = limpar.substring(0, limpar.length - 1)    
+    }
+    else if(botao == "ce"){
+
+        if(valorApagar.length > 1){
+            document.querySelector("#resultclick").innerHTML = limpar.substring(0, limpar.length - valorApagar[posicao].length)
+        }
+        else{
+            document.querySelector("#resultclick").innerHTML = ""
+        }
+    }
+}
