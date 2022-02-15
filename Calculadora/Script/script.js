0b564c7282360b70a59fc7454c457cc5c62eba7e
Mr-kzen <lukaszzzen@gmail.com>
2022-02-15 04:35:13 -0300

    Passos finais com a calculadora! modificação das funções raizQ, x2 e 1/!

--- Calculadora/Script/script.js
+++ Calculadora/Script/script.js
@@ -9,82 +9,104 @@
 const valor = 0
 const caracter = ['+', '-', '*', '/']
 
-function calcular(click){// imprime o que está sendo feito!
+function calcular(click) {// imprime o que está sendo feito!
 
-    if(click == "."){
-        click =`,`
+    const resultado = document.querySelector("#resultclick").innerHTML
+
+    if (click == ".") {
+        click = `,`
     }
-    if(document.querySelector("#result").innerHTML.search('=')>0){
+
+    if (click == "*"){
+        click = `x`
+    }
+
+    if (document.querySelector("#result").innerHTML.search('=') >= 0) {
         document.querySelector("#result").innerHTML = ""
         document.querySelector("#resultclick").innerHTML = ""
     }
-    if(click == "+" || click == "-" || click == "*" || click == "/"){
+
+    if(click == "xQd"){
+        document.querySelector("#result").innerHTML += "sqr( "+ document.querySelector("#resultclick").innerHTML + " )="
+        resultado = Number(resultado) * Number(resultado)
+        document.querySelector("#resultclick").innerHTML = resultado.toFixed(15)
+    } 
+    else if(click == "pct"){
+        document.querySelector("#resultclick").innerHTML = document.querySelector("#resultclick").innerHTML / 100
+    } 
+    else if(click == "1/x"){
+        document.querySelector("#result").innerHTML += "1/( "+ document.querySelector("#resultclick").innerHTML + " )="
+        document.querySelector("#resultclick").innerHTML = 1 / Number(resultado)
+    }
+    else if (click == "raiz2") {
+
+        document.querySelector("#result").innerHTML += "raiz( "+ document.querySelector("#resultclick").innerHTML + " )="
+        document.querySelector("#resultclick").innerHTML = Math.sqrt(Number(resultado))
+    }else if (click == "+" || click == "-" || click == "x" || click == "/") {
         document.querySelector("#result").innerHTML += document.querySelector("#resultclick").innerHTML + click
         document.querySelector("#resultclick").innerHTML = ""
-    }else if(click){
+    }else if (click) {
         document.querySelector("#resultclick").innerHTML += click
     }
-    
 }
 
-function result(){//retorna o resultado final
-    
-    document.querySelector("#result").innerHTML += document.querySelector("#resultclick").innerHTML
-    
-    const resultClick = document.querySelector("#resultclick").innerHTML
-    var resultado = document.querySelector("#result").innerHTML 
-    
-    resultado.replace(",", ".");
-    // resultado.replace("=", "");
+function result() {//retorna o resultado final
 
-    if(resultado){
-        document.querySelector("#resultclick").innerHTML = eval(resultado)
-        document.querySelector("#result").innerHTML += '='
+    var resultado = document.querySelector("#result").innerHTML
+
+    resultado.replace(",", ".");
+    resultado.replace("x", "*")
+
+    if (resultado) {
+
+        console.log(resultado.search("raiz") > 0 || resultado.search("1/") > 0)
+        if (resultado.search("r") >= 0) {
+
+        } else {
+            document.querySelector("#result").innerHTML += document.querySelector("#resultclick").innerHTML
+
+            document.querySelector("#resultclick").innerHTML = eval(resultado)
+            document.querySelector("#result").innerHTML += '='
+        }
 
         // historico
 
-            //-- Criando os elementos que serão inseridos
+        //-- Criando os elementos que serão inseridos
 
-                const container = document.querySelector(".historico")
+        const container = document.querySelector(".historico")
 
-                const novaDiv = document.createElement("div")
+        const novaDiv = document.createElement("div")
 
-                const p1 = document.createElement("p")       
-                p1.appendChild(document.createTextNode(resultado))
-                novaDiv.appendChild(p1)
+        const p1 = document.createElement("p")
+        p1.appendChild(document.createTextNode(resultado))
+        novaDiv.appendChild(p1)
 
-                const p2 = document.createElement("p")
-                p2.appendChild(document.createTextNode(eval(resultado)))
-                novaDiv.appendChild(p2)
-                
-                container.appendChild(novaDiv)
+        const p2 = document.createElement("p")
+        p2.appendChild(document.createTextNode(document.querySelector("#resultclick").innerHTML))        
+        novaDiv.appendChild(p2)
 
-                document.querySelector("#historicos").classList.add('sr-only')
-                novaDiiv.classList.add('historicos')
+        container.appendChild(novaDiv)
+
+        document.querySelector("#historicos").classList.add('sr-only')
+        novaDiv.classList.add('historicos')
     }
-    else{
+    else {
         document.querySelector("#resultclick").innerHTML = ""
     }
 }
 
-function clean(botao){
+function clean(botao) {
 
     limpar = document.querySelector("#resultclick").innerHTML
 
-    if(botao == 'c'){
+    if (botao == 'c') {
         document.querySelector("#resultclick").innerHTML = ""
         document.querySelector("#result").innerHTML = ""
     }
-    else if(botao == "delete"){
-        document.querySelector("#resultclick").innerHTML = limpar.substring(0, limpar.length - 1)    
+    else if (botao == "delete") {
+        document.querySelector("#resultclick").innerHTML = limpar.substring(0, limpar.length - 1)
     }
-    else if(botao == "ce"){
-
-        if(valorApagar.length > 1){
-            document.querySelector("#resultclick").innerHTML = limpar.substring(0, limpar.length - valorApagar[posicao].length)
-        }
-        else{
-            document.querySelector("#resultclick").innerHTML = ""
-        }
+    else if (botao == "ce") {
+        document.querySelector("#resultclick").innerHTML = ""
     }
 }
