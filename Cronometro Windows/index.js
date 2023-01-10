4794a6fdabad73480effb06c1fe7aa6e1d8f97bc
Mr-kzen <lukaszzzen@gmail.com>
2023-01-10 19:52:08 -0300

    Adicionanod projeto pessoal Cronometro

---
+++ Cronometro Windows/index.js
@@ -0,0 +1,60 @@
+const listSelector = document.querySelectorAll("[data-selection]")
+const li = document.querySelectorAll(".addTimer ul li")
+
+li.forEach((event) => {
+  
+  event.addEventListener('click', () => {
+   
+    const hour = event.classList.contains('hour')
+    const minute = event.classList.contains('minute')
+    const seconts = event.classList.contains('seconts')
+    
+    const ifTimerSelection = document.querySelectorAll(".timerSelection")
+    
+  ifTimerSelection.forEach( () => {
+    
+    console.log("entrei" + ifTimerSelection)
+    if(ifTimerSelection[0].classList.contains){
+      ifTimerSelection[0].classList.remove("timerSelection")
+    }
+
+  })
+    
+    console.log("Timer " + hour)
+    
+    if(hour){
+      event.classList.add("timerSelection")  
+    }if(minute){
+      event.classList.add("timerSelection")
+    }if(seconts){
+      event.classList.add("timerSelection")
+    }
+    
+    
+  })
+  
+ 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//ativando painel para novo cronometro
+
+const newCro = document.querySelectorAll(".adc")
+
+newCro.addEventListener("click", () => {
+  
+})
