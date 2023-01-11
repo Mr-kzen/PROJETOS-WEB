0c8761a6edb9c52ea3bac77e3a77ac0011485cd3
Mr-kzen <lukaszzzen@gmail.com>
2023-01-11 08:02:08 -0300

    Ultimas modificações no containeir pai!

--- nlw6 - Discover/src/db/config.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require("sqlite3"); //importando os modulos
-const {  open  } = require("sqlite");
-
-module.exports = () =>
-        open({
-            filename: './src/db/rocketq.sqlite',
-            driver: sqlite3.Database,
-        })
